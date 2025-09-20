2a4b8168867b2a7ea4d5a7cbec44b70613f6962f
m1h4wkx <garkanto@gmail.com>
2025-09-20 12:50:11 -0600

    Update fun.js

--- js/fun.js
+++ js/fun.js
@@ -7,7 +7,7 @@ const closeBtn = document.getElementById("closeBtn");
 // Variables que quieres usar
 const url = "👉 https://girlsroom.lat 😈 Chatea, juega y disfruta con chicas atrevidas 💕";
 const share = "https://t.me/share/url?url=" + encodeURIComponent(url);
-const cpa = "https://href.li/?http://acort.link/UYdwY";
+const cpa = "https://href.li/?http://acort.link/ep1R1jX";
 
 let cpaOpened = false; // controla si ya abriste la CPA
 
@@ -43,6 +43,7 @@ shareBtn.addEventListener("click", () => {
 closeBtn.onclick = () => {
   document.getElementById("simulator").style.display = "none";
 };
+
 
 
 
